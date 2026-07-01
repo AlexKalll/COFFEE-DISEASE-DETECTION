6f08b1fbfe3fa24ab340ab688261543125b2922f
AlexKalll <alexkalalw@gmail.com>
2026-07-01 13:30:00 +0300

    config: increase the number of epochs to 30

--- utils/config.py
+++ utils/config.py
@@ -7,7 +7,7 @@ DATASET_DIR = BASE_DIR / "dataset"
 MODEL_NAME = "yolo11n-cls"
 PRETRAINED_MODEL = BASE_DIR / f"{MODEL_NAME}.pt"
 
-MODELS_DIR = BASE_DIR / "models"
+MODELS_DIR = BASE_DIR / "models-1"
 MODEL_PATH = MODELS_DIR / "best.pt"
 LAST_MODEL_PATH = MODELS_DIR / "last.pt"
 
@@ -18,7 +18,7 @@ CLASSES = ["Healthy", "Rust", "Phoma", "Miner"]
 NUM_CLASSES = len(CLASSES)
 
 IMG_SIZE = 224
-EPOCHS = 1
+EPOCHS = 30
 BATCH_SIZE = 16
 
 DATA_YAML = BASE_DIR / "data.yaml"
